b5c03b464fde6f5f19498a493bb8050d5ede17bd
LiviaGouvea <gouvea.lc@gmail.com>
2021-11-05 14:54:21 -0300

    Insere biblioteca lcobucci para gerar token jwt para sala do Jitsi

--- unifop-hook-mail.php
+++ unifop-hook-mail.php
@@ -12,11 +12,45 @@
  * Domain Path:       /languages
  */
 
+use Lcobucci\JWT\Configuration;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
+use Lcobucci\JWT\Signer\Key\InMemory;
 
-add_filter('wp_mail','redirect_mails', 10,1);
-function redirect_mails($args){
+if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
+    require __DIR__ . '/vendor/autoload.php';
+}
+
+function generateJitsiUrl($userName, $userEmail, $roomName, $moderator)
+{
+    $config = Configuration::forSymmetricSigner(
+        new Sha256(),
+        InMemory::plainText(getenv('JITSI_SECRET'))
+    );
+
+    $token = $config->Builder()
+        ->permittedFor(getenv('JITSI_APPID')) //aud
+        ->issuedBy(getenv('JITSI_APPID')) // iss
+        ->relatedTo(getenv('JITSI_URL')) // sub
+        ->withClaim('room', $roomName) // room
+        ->withClaim('moderator', $moderator) // moderator
+        ->withClaim('context', [
+            'user' => [
+                'name' => $userName,
+                'email' => $userEmail
+                ]
+        ]) // room
+        ->getToken($config->signer(), $config->signingKey());
+    return 'https://' . getenv('JITSI_URL') .
+        '/' . $roomName.
+        '?jwt=' . $token->toString();
+}
+
+add_filter('wp_mail','insertLink', 10,1);
+function insertLink($args){
     if ($args["subject"] == "Seu pedido em Unifop foi concluído") {
-        $args["message"] = str_replace('{link_consulta}','https://link',$args["message"]);
+        $nome = strtok($args["to"], '@');
+        $url = generateJitsiUrl($nome, $args["to"], 'room-name', false);
+        $args["message"] = str_replace('{link_consulta}',"<a href='$url'>Link da consulta</a>",$args["message"]);
     }
     return $args;
 }
